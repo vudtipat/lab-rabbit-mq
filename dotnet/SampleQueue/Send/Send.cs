cf81ac72c400f1a6c265a2675409a6f24b52dee0
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-26 23:01:28 +0700

    simple-messaging

---
+++ dotnet/SampleQueue/Send/Send.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "dev", Password = "P@ssw0rd" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var message = "Hello World";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: string.Empty,
+    routingKey: "hello",
+    basicProperties: null,
+    body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
